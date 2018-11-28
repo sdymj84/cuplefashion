1b88b72e7a1ab8000d6450eb80ac44067653bd13
sdymj84 <sdymj84@gmail.com>
2018-11-27 20:55:10 -0600

    add card carousel examples
    Route to cardCarousel by typing 'localhost:3000/cardCarousel'.

--- routes/index.js
+++ routes/index.js
@@ -37,6 +37,7 @@ exports = module.exports = function (app) {
 	app.get('/', routes.views.index);
 	app.get('/gallery', routes.views.gallery);
 	app.all('/contact', routes.views.contact);
+	app.all('/cardCarousel', routes.views.cardCarousel);
 
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
 	// app.get('/protected', middleware.requireUser, routes.views.protected);
