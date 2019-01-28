3592768b081e6784963d2aa32ea24dc7d7ba50cd
byul5810 <dkim5@g.emporia.edu>
2019-01-28 05:41:15 -0600

    edit login logic

--- routes/views/login.js
+++ routes/views/login.js
@@ -1,5 +1,6 @@
 var keystone = require('keystone');
 var firebase = require('firebase');
+var customer = keystone.list('Customer');
 
 exports = module.exports = function (req, res) {
 
@@ -11,7 +12,6 @@ exports = module.exports = function (req, res) {
   locals.section = 'login';
 
   view.on('post', function (next) {
-    console.log('[routes/login.js] Login post function executed')
     const email = req.body.inputEmail;
     const password = req.body.inputPassword;
 
@@ -19,22 +19,35 @@ exports = module.exports = function (req, res) {
     // https://firebase.google.com/docs/reference/js/firebase.auth.Auth#signInWithEmailAndPassword
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(function (user) {
-        const uid = user.user.uid
-        console.log("User UID : " + uid)
+        const uuid = user.user.uid
+        console.log("User UID : " + uuid)
+        console.log('[routes/login.js] Login post function executed')
 
-        /* const docRef = db.collection('users').doc(uid)
-        docRef.get().then(doc => {
-          if (doc.exists) {
-            console.log("[routes/login.js] logged in and retreived user info successfully")
-            res.redirect('/')
-          } else {
-            console.log("No such document!")
-            next()
+        /*
+        difference between findOne() and find():
+        findOne - if query matches, first document is returned, otherwise null.
+        find - nomatter number of documents matched, a cursor is returned, never null.
+        */
+        customer.model.findOne({ uid: uuid }).exec(function (err, customer) {
+          if (err) {
+            console.log(err);
+            res.status(500).send('DB Error');
+            next();
           }
-        }).catch(err => {
-          console.log("Error getting document", err)
-          next(err)
-        }) */
+          if (!customer) {
+            console.log(" Couldn't find matching user");
+            res.status(500).send('NO customer uid matched with firebase in database');
+            next();
+          }
+          else {
+            locals.customer = customer;
+
+            console.log("[routes/login.js] logged in and retreived user info successfully")
+            console.log("[routes/login.js] Customer Information:" + customer);
+            res.redirect('/')
+          }
+
+        });
 
       }).catch(function (error) {
         console.log(error.message)
--- routes/views/main.js
+++ routes/views/main.js
@@ -1,6 +1,7 @@
 var keystone = require('keystone');
 var SiteInfo = keystone.list('SiteInfo');
 var firebase = require('firebase');
+var customer = keystone.list('Customer');
 
 exports = module.exports = function (req, res) {
 
@@ -25,7 +26,19 @@ exports = module.exports = function (req, res) {
 		if (user) {
 			console.log("[routes/main.js] Welcome customer! you're currently logged in");
 			locals.user = user
-			const uid = user.uid
+			const uuid = user.uid
+
+			customer.model.find({ uid: uuid }).exec(function (err, data) {
+				if (err) {
+					console.log(err);
+					res.status(500).send('DB Error');
+					next();
+				}
+				locals.customer = data;
+				console.log(data);
+				next();
+			});
+
 			/* const docRef = db.collection('users').doc(uid)
 			docRef.get().then(doc => {
 				if (doc.exists) {
